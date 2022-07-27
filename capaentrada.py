b4071e7e6b6ecca6b12282512f812a91f947619e
JordiSalas1120 <90120002+JordiSalas1120@users.noreply.github.com>
2022-07-26 23:34:07 -0400

    Add files via upload

--- capaentrada.py
+++ capaentrada.py
@@ -1,37 +1,19 @@
 import cv2 as cv
-import os
-import imutils
-modelo='FotosElon'
-ruta1='E:/Proyectos Python/curso/reconocimientofacial1'
-rutacompleta = ruta1 + '/'+ modelo
-if not os.path.exists(rutacompleta):
-    os.makedirs(rutacompleta)
+import numpy as np 
+camara = cv.VideoCapture(0)#abrir camara
 
-
-
-camara=cv.VideoCapture('ElonMusk.mp4')
-ruidos=cv.CascadeClassifier('E:\Proyectos Python\curso\entrenamientos opencv ruidos\opencv-master\data\haarcascades\haarcascade_frontalface_default.xml')
-id=0
+ruidos = cv.CascadeClassifier('C:/Users/Jordi/Desktop/Curso de python/proyectos_ia/reconocimientofacial/haarcascade_frontalface_default.xml')
 while True:
-    respuesta,captura=camara.read()
-    if respuesta==False:break
-    captura=imutils.resize(captura,width=640)
-
+    _,captura=camara.read()#captura  de la camara
     grises=cv.cvtColor(captura, cv.COLOR_BGR2GRAY)
-    idcaptura=captura.copy()
-
-    cara=ruidos.detectMultiScale(grises,1.3,5)
-
-    for(x,y,e1,e2) in cara:
-        cv.rectangle(captura, (x,y), (x+e1,y+e2), (0,255,0),2)
-        rostrocapturado=idcaptura[y:y+e2,x:x+e1]
-        rostrocapturado=cv.resize(rostrocapturado, (160,160),interpolation=cv.INTER_CUBIC)
-        cv.imwrite(rutacompleta+'/imagen_{}.jpg'.format(id), rostrocapturado)
-        id=id+1
+    cara=ruidos.detectMultiScale(grises, 1.3, 5)#reconocimiento de caras. el 1 significa el 100 por ciento. 1.5 seria el 50 por ciento
     
-    cv.imshow("Resultado rostro", captura)
+    for (x,y,e1,e2) in cara:#recorre todos los puntos de lcara
+        cv.rectangle(captura, (x,y), (x+e1,y+e2),(0, 255, 0),2)
+    cv.imshow("Resultado Rostro", captura)
 
-    if id==351:
+    if cv.waitKey(1) == ord('q'):
         break
-camara.release()
-cv.destroyAllWindows()
+    
+    camara.release()#release destruye vualquier vemtama abierta
+    cv.destroyAllWindows()    
